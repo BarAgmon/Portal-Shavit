eaf8835064f08daabd958b295aeeda9bbc342754
Bar Agmon <barush885@gmail.com>
2020-03-23 15:25:59 +0200

    code refactor - allappcards component created

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import AppCard from './views/AppCard';
-import Jenkinspic from './images/jenkins.png';
-import Dockerpic from './images/docker.png';
-import RocketChatpic from './images/rocketchat.png';
-import Nexuspic from './images/nexus.png';
-import Openshiftpic from './images/openshift.png';
-import Splunkpic from './images/splunk.jpg';
+import AllAppCards from './views/AllAppCards';
 
 function App() {
   return (
     <AppDesign className="App">
-      <AllAppCards>
-        <AppCard title='Jenkins' image={Jenkinspic}/>
-        <AppCard title='Docker' image={Dockerpic}/>
-        <AppCard title='RocketChat' image={RocketChatpic}/>
-        <AppCard title='Nexus' image={Nexuspic}/>
-        <AppCard title='Openshift' image={Openshiftpic}/>   
-        <AppCard title='Splunk' image={Splunkpic}/>   
-      </AllAppCards>
+      <AllAppCards/>
     </AppDesign>
   );
 }
@@ -34,13 +21,3 @@ const AppDesign = styled.div`
   color: white;
 `;
 
-const AllAppCards= styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content:space-around;
-  align-items: center;
-  margin-right:2em;
-  margin-left: 2em;
-  height: 100%;
-  width: 100%;
-`
---
+++ Client/src/views/AllAppCards.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import AppCard from './AppCard';
+import Jenkinspic from '../images/jenkins.png';
+import Dockerpic from '../images/docker.png';
+import RocketChatpic from '../images/rocketchat.png';
+import Nexuspic from '../images/nexus.png';
+import Openshiftpic from '../images/openshift.png';
+import Splunkpic from '../images/splunk.jpg';
+
+export default function AllAppCards() {
+  return (
+      <AllAppCardsDesign>
+        <AppCard title='Jenkins' image={Jenkinspic}/>
+        <AppCard title='Docker' image={Dockerpic}/>
+        <AppCard title='RocketChat' image={RocketChatpic}/>
+        <AppCard title='Nexus' image={Nexuspic}/>
+        <AppCard title='Openshift' image={Openshiftpic}/>   
+        <AppCard title='Splunk' image={Splunkpic}/>   
+      </AllAppCardsDesign>
+  );
+}
+
+
+const AllAppCardsDesign= styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content:space-around;
+  align-items: center;
+  margin-right:2em;
+  margin-left: 2em;
+  height: 100%;
+  width: 100%;
+`
