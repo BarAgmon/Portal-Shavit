e19b14b1cf546ef769ae1ecfa609ebde9d4d4780
Maya Rachmani <maya3rm@gmail.com>
2020-04-12 16:51:11 +0300

    changed some things

--- Client/src/views/Sidebar/MenuItem.js
+++ Client/src/views/Sidebar/MenuItem.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {Icon, Menu} from "semantic-ui-react";
+import { Link } from 'react-router-dom';
 
 export default function MenuItem ({iconName, iconText, path}) {
     return (
-        <Menu.Item as='NavLink' to={path}>
+        <Menu.Item as='a' to={path}>
             <Icon name={iconName} />
             {iconText}
         </Menu.Item>
--- Client/src/views/Sidebar/Sidebar.js
+++ Client/src/views/Sidebar/Sidebar.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Menu, Sidebar } from 'semantic-ui-react';
 import '../../Semantic-UI-CSS-master/semantic.min.css';
 import MenuItem from './MenuItem';
+import Database from "../DBsControl/HomePage";
+import DatabaseBackup from "../DBsControl/BackupPage";
+import Links from "../Links/HomePage";
 
 const SidebarApp = () => {
     return(
@@ -12,10 +15,10 @@ const SidebarApp = () => {
             icon='labeled'
             inverted
             vertical
-            visible='true'
+            visible
             width='thin'
         >
-            <MenuItem iconName='database' iconText='Backup and restore' path='/backup'/>
+            <MenuItem iconName='database' iconText='Backup and restore' path='/backup' link={Database}/>
             <MenuItem iconName='dochub' iconText='Documentation'/>
             <MenuItem iconName='docker' iconText='Docker CLI'/>
         </Sidebar>
